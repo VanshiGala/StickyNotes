19196ac2e780c9c059178cee09eeafb43ad40944
karan <karannisar811@gmail.com>
2025-06-18 18:36:48 +0530

    changes done in work page counter

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 
 function Work() {
-  let [todos, setTodos] = useState([{ task: "sample Task", id: uuidv4(), isDone: false },]);
+  let [todos, setTodos] = useState([]);
   let [newTodo, setNewTodo] = useState("");
 
   let addNewtask = () => {
@@ -26,7 +26,7 @@ function Work() {
   let markAsDone = (id) => {
     setTodos((prevTodos) => {
       return prevTodos.map((todo) => {
-        if (todo.id == id) {
+        if (todo.id === id) {
           return {
             ...todo,
             isDone: true,
@@ -39,20 +39,20 @@ function Work() {
   };
   return (
     <div className="p-4 max-w-md mx-auto text-black">
-      <h1 className="font-bold mt-4 text-4xl ">Work</h1>
+      <h1 className="font-bold mt-4 text-4xl ">Work<span className="text-xlg ml-30">{todos.length}</span></h1>
       <br></br>
       <div>
         <input
         placeholder="add a task"
         value={newTodo}
         onChange={updateTodoValue}
-      ></input> <span></span>
-      <button onClick={addNewtask}> + </button>
+        className="border border-grey-300 rounded p-2 flex-1 mr-2"
+        ></input> <span></span>
+      <button onClick={addNewtask} className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600"> + </button>
       <br></br>
       <br></br>
 
       <hr></hr>
-      <h4>Tasks Todo</h4>
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
@@ -62,9 +62,11 @@ function Work() {
               {todo.task}
             </span>
             &nbsp;&nbsp;&nbsp;
-            <button onClick={() => deleteTodo(todo.id)}>delete</button>{" "}
+            <button onClick={() => deleteTodo(todo.id)} className="p-1"> ❌  </button>{" "}
             <span></span>
-            <button onClick={() => markAsDone(todo.id)}>Mark As Done</button>
+            {/* <button onClick={() => markAsDone(todo.id)}>Mark As Done</button> */}
+            <input type = "checkbox" checked={todo.isDone} onChange={()=>
+              markAsDone(todo.id)} className=""/>
           </li>
         ))}
       </ul>
